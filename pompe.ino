55e5d75837ac7d047ef3ecda941cce3aac14197e
hamouBerki <166293165+hamouBerki@users.noreply.github.com>
2025-03-13 11:15:30 +0100

    Add files via upload

--- pompe.ino
+++ pompe.ino
@@ -1,6 +1,6 @@
 #define POMPE_PIN 9  // Broche PWM connectée à la gate du MOSFET
 
-/* ce code est concus pour tester la pompe sous differentes vitesses (utiliser l'alim HAMEG)
+/* ce code est concus pour tester la pompe sous differentes vitesses sans les bouttons (utiliser l'alim HAMEG)
         Rq : si la pompe ne fonctionne pas comme attendu vérifier la connectique, puis les composants 
         en dernier recours suivre le signal PWM avec un oscillo pour reperer où se situe le probleme
 */
@@ -12,7 +12,7 @@ void setup() {
 
 void loop() {
   Serial.println("tourner");
-  analogWrite(POMPE_PIN, 150);  // max :255
+  analogWrite(POMPE_PIN, 250);  // max :255
   delay(3000);
 
 
